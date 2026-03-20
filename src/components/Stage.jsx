96da9c91e54ad90eff330ea5cee2f30f60c0bd5f
mahmoudAndRanim <mahmoudabouchandi@gmail.com>
2026-03-20 16:23:25 +0100

    style: clean sofa, taller dance floor

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -5,13 +5,7 @@ export default function Stage() {
       <div className="stage__area">
         <div className="kouch">
           <div className="kouch__back" />
-          <div className="kouch__seat">
-            <div className="kouch__content">
-              <div className="kouch__icon">💍</div>
-              <div className="kouch__label">Brudeparret</div>
-              <div className="kouch__sub">Mahmoud & Raneem</div>
-            </div>
-          </div>
+          <div className="kouch__seat" />
           <div className="kouch__arm kouch__arm--left" />
           <div className="kouch__arm kouch__arm--right" />
         </div>
